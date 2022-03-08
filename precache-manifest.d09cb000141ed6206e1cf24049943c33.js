cf3f9590bcc3449eaf2f280ada3ee29c5c88ec90
Natália Capra Ferrazzo <nataliaferrazzo@hotmail.com>
2022-03-08 00:51:52 -0300

    Updates

---
+++ precache-manifest.d09cb000141ed6206e1cf24049943c33.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "217cc32baaeae73375264814a4172b00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44d76bb4a5be4aa6f432",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44d76bb4a5be4aa6f432",
+    "url": "/static/js/main.af6ed386.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "6689d92ec6fa02dd9882907b51739389",
+    "url": "/static/media/400x400.6689d92e.jpeg"
+  },
+  {
+    "revision": "38a1c2acce4cd25fb3fe24d7b0636cb2",
+    "url": "/static/media/resume.38a1c2ac.pdf"
+  }
+]);
